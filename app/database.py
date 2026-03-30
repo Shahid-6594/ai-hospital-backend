998783a4040087b9dbf9f42018d62950a4f7057d
Shahid-6594 <alishahid57252@gmail.com>
2026-03-30 22:40:49 +0530

    fixed database connection

--- app/database.py
+++ app/database.py
@@ -1,19 +1,19 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DATABASE_URL = "postgresql://postgres:Current%408744@localhost:5432/ai_hospital"
 DATABASE_URL = os.getenv("DATABASE_URL")
-if DATABASE_URL is None:
-    DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL_INTERNAL = os.getenv("DATABASE_URL_INTERNAL")
 
-engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False} if "sqlite" in DATABASE_URL else {})
+# Use internal DB when on Render, else external for local
+if DATABASE_URL_INTERNAL:
+    engine = create_engine(DATABASE_URL_INTERNAL)
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
